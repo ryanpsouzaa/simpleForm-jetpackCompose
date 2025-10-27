dd0fa877d59754690524f4973266834760796d63
ryanpsouza <ryanpsouzagit@gmail.com>
2025-10-27 19:55:59 -0300

    First Commit - implementing form with remember state

---
+++ app/src/main/java/com/app/simpleform/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.app.simpleform
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.app.simpleform.ui.theme.SimpleFormTheme
+import com.app.simpleform.ui.theme.Typography
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimpleFormTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HelloContent(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var name by remember { mutableStateOf("") }
+
+        if(name.isNotEmpty()){
+            Text(
+                text = stringResource(id = R.string.hello, name),
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = Typography.bodyMedium
+            )
+
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = { updatedText -> name = updatedText },
+            label = { Text(text = stringResource(id = R.string.name)) }
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun HelloContentPreview() {
+
+}
